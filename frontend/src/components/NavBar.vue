180ad72304abd38aaf5908ca86a1076f8038ed56
giuliomuraro <giuliomuraro97@gmail.com>
2023-05-19 20:09:35 +0200

    Restauro repo, dopo errori nel push. Finalizzazione navbar(no hamburger menù)

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div v-if="$route.path == '/'" class="bg-gray-800 h-20 flex justify-center items-center">
+      <img src="../images/logo.jpeg" alt="Logo" class="max-h-full max-w-80">
+    </div>
+    <div v-else class="bg-gray-800 h-20 flex justify-start items-center">
+      <img src="../images/logoEnlarged.png" alt="Logo" class="max-h-full max-w-80 ml-20">
+      <h1 v-if="$route.path == '/LoginView'" class="text-3xl text-white ml-3">LogIn</h1>
+      <h1 v-else-if="$route.path == '/SignUpView'" class="text-3xl text-white ml-3">SignUp</h1>
+      <h1 v-else-if="$route.path == '/BookingView'" class="text-3xl text-white ml-3">Booking</h1>
+      <h1 v-else-if="$route.path == '/AdminAccountView'" class="text-3xl text-white ml-3">Admin Account</h1>
+      <h1 v-else-if="$route.path == '/ProfileView'" class="text-3xl text-white ml-3">Profile</h1>
+      <OptionsButton class="ml-auto mr-5"/>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import OptionsButton from './OptionsButton.vue'
+
+export default {
+  name: 'NavBar',
+  components:{
+    OptionsButton
+  }
+    
+  
+}
+
+</script>
---
+++ frontend/src/components/OptionsButton.vue
@@ -0,0 +1,14 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 20 20" stroke="white">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+    </svg>
+</template>
+    
+<script>
+    
+export default {
+    name: 'OptionsButton',
+}
+
+</script>
+    
